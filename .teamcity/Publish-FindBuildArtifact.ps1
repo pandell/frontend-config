bb94bdbb32890342427c92c6f965ad88529b1090
Milan Gardian <mgardian@gmail.com>
2024-05-10 15:58:30 -0600

    Update formatting and indentation of editor/settings files

--- .teamcity/Publish-FindBuildArtifact.ps1
+++ .teamcity/Publish-FindBuildArtifact.ps1
@@ -1,14 +1,14 @@
 #requires -Version 7
 
 Param(
-  [parameter(Mandatory = $true)]
-  [long]$BuildCounter,
+    [parameter(Mandatory = $true)]
+    [long]$BuildCounter,
 
-  [parameter(Mandatory = $true)]
-  [string]$SelectedPackage,
+    [parameter(Mandatory = $true)]
+    [string]$SelectedPackage,
 
-  [parameter(Mandatory = $true)]
-  [string]$SelectedTag
+    [parameter(Mandatory = $true)]
+    [string]$SelectedTag
 )
 
 # initialize
@@ -16,31 +16,31 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 . $PSScriptRoot/Lib-TeamCity.ps1
 trap {
-  Write-TeamCityError "Error" $_ -ReportAsBuildProblem
-  Write-TeamCityBuildStatus "Failed to find build artifact" -Failure
-  exit 1
+    Write-TeamCityError "Error" $_ -ReportAsBuildProblem
+    Write-TeamCityBuildStatus "Failed to find build artifact" -Failure
+    exit 1
 }
 
 # find the artifact
 Set-TeamCityBuildNumber "$BuildCounter | $SelectedPackage"
 $artifact = @(Get-ChildItem build/$SelectedPackage*.tgz)
 if ($artifact.Count -lt 1) {
-  Write-Error "No build artifact for `"$SelectedPackage`""
+    Write-Error "No build artifact for `"$SelectedPackage`""
 }
 if ($artifact.Count -gt 1) {
-  Write-Error "Too many build artifacts for `"$SelectedPackage`""
+    Write-Error "Too many build artifacts for `"$SelectedPackage`""
 }
 
 # extract version from artifact name
 $artifactBaseName = $artifact[0].BaseName
 $artifactPath = $artifact[0].FullName
 $artifactVersion = if ($artifactBaseName -match "^$($SelectedPackage)_(.+)$") {
-  $Matches[1]
+    $Matches[1]
 } else {
-  $null
+    $null
 }
 if (-not $artifactVersion) {
-  Write-Error "Missing version in `"$artifactBaseName`""
+    Write-Error "Missing version in `"$artifactBaseName`""
 }
 
 # persist results for subsequent build step(s)
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,13 +1,12 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.BuildSteps
-import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.BuildTypeSettings
 import jetbrains.buildServer.configs.kotlin.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.RelativeId
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildSteps.PowerShellStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.exec
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
-import jetbrains.buildServer.configs.kotlin.buildSteps.PowerShellStep
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.ui.*
 
@@ -26,12 +25,12 @@ version = "2023.11"
 
 // prefixes of packages included in this monorepo
 enum class ConfigPackagePrefix {
-  browserslist,
-  eslint,
-  jest,
-  prettier,
-  stylelint,
-  typescript
+    browserslist,
+    eslint,
+    jest,
+    prettier,
+    stylelint,
+    typescript,
 }
 
 // calculates full package name from the specified prefix
@@ -39,151 +38,154 @@ fun packageNameFromPrefix(packagePrefix: ConfigPackagePrefix) = "$packagePrefix-
 
 // runs "yarn pack" for the workspace with the specified package
 fun BuildSteps.execYarnPack(packagePrefix: ConfigPackagePrefix) {
-  var target = packageNameFromPrefix(packagePrefix)
+    var target = packageNameFromPrefix(packagePrefix)
 
-  exec {
-    name = "Pack $target"
-    path = "yarn"
-    arguments = "workspace @pandell/$target pack --out ../../${target}_%%v.tgz"
-  }
+    exec {
+        name = "Pack $target"
+        path = "yarn"
+        arguments = "workspace @pandell/$target pack --out ../../${target}_%%v.tgz"
+    }
 }
 
 // =============================================================================
 
 project {
-  params {
-    param("env.Path", "%env.Path%;%env.NodeRoot20100%")
-  }
-
-  // ---------------------------------------------------------------------------
-  // build and pack all packages included in this monorepo
-  // note: resulting ".tgz" archives are captured as TeamCity artifacts,
-  // to be used in publish build configuration
-  buildType {
-    id("Build")
-    name = "1. Build"
-    artifactRules = "*.tgz"
-
-    vcs {
-      root(DslContext.settingsRoot)
-    }
-
-    triggers {
-      vcs { }
-    }
-
-    features {
-      commitStatusPublisher {
-        publisher = github {
-          githubUrl = "https://api.github.com"
-          authType = vcsRoot()
-        }
-      }
-    }
-
     params {
-      param("env.PANDELL_NPM_TOKEN", "%pandell.npmToken.access%")
+        param("env.Path", "%env.Path%;%env.NodeRoot20100%")
     }
 
-    steps {
-      exec {
-        name = "Install tooling"
-        path = "yarn"
-        arguments = "install --immutable"
-      }
-      exec {
-        name = "Print tool versions"
-        path = "yarn"
-        arguments = "run versions"
-      }
-      exec {
-        name = "Check format (prettier)"
-        path = "yarn"
-        arguments = "run format"
-      }
-      execYarnPack(ConfigPackagePrefix.browserslist)
-      execYarnPack(ConfigPackagePrefix.eslint)
-      execYarnPack(ConfigPackagePrefix.jest)
-      execYarnPack(ConfigPackagePrefix.prettier)
-      execYarnPack(ConfigPackagePrefix.stylelint)
-      execYarnPack(ConfigPackagePrefix.typescript)
-    }
-  }
+    // ---------------------------------------------------------------------------
+    // build and pack all packages included in this monorepo
+    // note: resulting ".tgz" archives are captured as TeamCity artifacts,
+    // to be used in publish build configuration
+    buildType {
+        id("Build")
+        name = "1. Build"
+        artifactRules = "*.tgz"
 
-  // ---------------------------------------------------------------------------
-  // publish selected package using the selected tag
-  // note: package is not rebuilt at this time, we use the ".tgz" file
-  // that was produced during build and captured in artifacts
-  buildType {
-    id("Publish")
-    name = "2. Publish"
-    type = BuildTypeSettings.Type.DEPLOYMENT
-    enablePersonalBuilds = false
-    maxRunningBuilds = 1
-
-    vcs {
-      root(DslContext.settingsRoot)
-    }
-
-    dependencies {
-      dependency(RelativeId("Build")) {
-        snapshot {
-          onDependencyFailure = FailureAction.FAIL_TO_START
+        params {
+            param("env.PANDELL_NPM_TOKEN", "%pandell.npmToken.access%")
         }
 
-        artifacts {
-          cleanDestination = true
-          artifactRules = "%selectedPackage%*.tgz => build"
+        vcs {
+            root(DslContext.settingsRoot)
         }
-      }
+
+        triggers {
+            vcs { }
+        }
+
+        features {
+            commitStatusPublisher {
+                publisher =
+                    github {
+                        githubUrl = "https://api.github.com"
+                        authType = vcsRoot()
+                    }
+            }
+        }
+
+        steps {
+            exec {
+                name = "Install tooling"
+                path = "yarn"
+                arguments = "install --immutable"
+            }
+            exec {
+                name = "Print tool versions"
+                path = "yarn"
+                arguments = "run versions"
+            }
+            exec {
+                name = "Check format (prettier)"
+                path = "yarn"
+                arguments = "run format"
+            }
+            execYarnPack(ConfigPackagePrefix.browserslist)
+            execYarnPack(ConfigPackagePrefix.eslint)
+            execYarnPack(ConfigPackagePrefix.jest)
+            execYarnPack(ConfigPackagePrefix.prettier)
+            execYarnPack(ConfigPackagePrefix.stylelint)
+            execYarnPack(ConfigPackagePrefix.typescript)
+        }
     }
 
-    params {
-      select(
-        "selectedPackage",
-        "",
-        label = "Package to publish",
-        display = ParameterDisplay.PROMPT,
-        options = listOf(
-          packageNameFromPrefix(ConfigPackagePrefix.browserslist),
-          packageNameFromPrefix(ConfigPackagePrefix.eslint),
-          packageNameFromPrefix(ConfigPackagePrefix.jest),
-          packageNameFromPrefix(ConfigPackagePrefix.prettier),
-          packageNameFromPrefix(ConfigPackagePrefix.stylelint),
-          packageNameFromPrefix(ConfigPackagePrefix.typescript)
-        )
-      )
-      text(
-        "selectedTag",
-        "latest",
-        label = "NPM tag",
-        display = ParameterDisplay.PROMPT
-      )
-      param("packageBuildArtifactFullPath", "")
-      param("env.NPM_CONFIG_//registry.npmjs.org/:_authToken", "%pandell.npmToken.publish%")
-    }
+    // ---------------------------------------------------------------------------
+    // publish selected package using the selected tag
+    // note: package is not rebuilt at this time, we use the ".tgz" file
+    // that was produced during build and captured in artifacts
+    buildType {
+        id("Publish")
+        name = "2. Publish"
+        type = BuildTypeSettings.Type.DEPLOYMENT
+        enablePersonalBuilds = false
+        maxRunningBuilds = 1
 
-    steps {
-      exec {
-        name = "Print tool versions"
-        path = "cmd"
-        arguments = """/c echo "[pwsh]" && pwsh --version && echo: && echo "[npm]" && npm --version"""
-      }
-      powerShell {
-        name = "Find package build artifact"
-        edition = PowerShellStep.Edition.Core
-        scriptMode = file { path = ".teamcity/Publish-FindBuildArtifact.ps1" }
-        scriptArgs = """
-          -BuildCounter %build.counter%
-          -SelectedPackage "%selectedPackage%"
-          -SelectedTag "%selectedTag%"
-        """.trimIndent()
-      }
-      exec {
-        name = "Publish package build artifact"
-        path = "npm"
-        arguments = """publish --access public --tag "%selectedTag%" "%packageBuildArtifactFullPath%" """
-      }
+        params {
+            select(
+                "selectedPackage",
+                "",
+                label = "Package to publish",
+                display = ParameterDisplay.PROMPT,
+                options =
+                    listOf(
+                        packageNameFromPrefix(ConfigPackagePrefix.browserslist),
+                        packageNameFromPrefix(ConfigPackagePrefix.eslint),
+                        packageNameFromPrefix(ConfigPackagePrefix.jest),
+                        packageNameFromPrefix(ConfigPackagePrefix.prettier),
+                        packageNameFromPrefix(ConfigPackagePrefix.stylelint),
+                        packageNameFromPrefix(ConfigPackagePrefix.typescript),
+                    ),
+            )
+            text(
+                "selectedTag",
+                "latest",
+                label = "NPM tag",
+                display = ParameterDisplay.PROMPT,
+            )
+            param("packageBuildArtifactFullPath", "")
+            param("env.NPM_CONFIG_//registry.npmjs.org/:_authToken", "%pandell.npmToken.publish%")
+        }
+
+        vcs {
+            root(DslContext.settingsRoot)
+        }
+
+        dependencies {
+            dependency(RelativeId("Build")) {
+                snapshot {
+                    onDependencyFailure = FailureAction.FAIL_TO_START
+                }
+
+                artifacts {
+                    cleanDestination = true
+                    artifactRules = "%selectedPackage%*.tgz => build"
+                }
+            }
+        }
+
+        steps {
+            exec {
+                name = "Print tool versions"
+                path = "cmd"
+                arguments = """/c echo "[pwsh]" && pwsh --version && echo: && echo "[npm]" && npm --version"""
+            }
+            powerShell {
+                name = "Find package build artifact"
+                edition = PowerShellStep.Edition.Core
+                scriptMode = file { path = ".teamcity/Publish-FindBuildArtifact.ps1" }
+                scriptArgs =
+                    """
+                    -BuildCounter %build.counter%
+                    -SelectedPackage "%selectedPackage%"
+                    -SelectedTag "%selectedTag%"
+                    """.trimIndent()
+            }
+            exec {
+                name = "Publish package build artifact"
+                path = "npm"
+                arguments = """publish --access public --tag "%selectedTag%" "%packageBuildArtifactFullPath%" """
+            }
+        }
     }
-  }
 }
