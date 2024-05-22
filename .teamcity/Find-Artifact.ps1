716e25c64d0efb2b7fe1256f3636759e22763765
Milan Gardian <mgardian@gmail.com>
2024-05-21 19:26:28 -0600

    Update Kotlin DSL to follow MobileWebApi style

--- .teamcity/Find-Artifact.ps1
+++ .teamcity/Find-Artifact.ps1
@@ -5,10 +5,10 @@ Param(
     [long]$BuildCounter,
 
     [parameter(Mandatory = $true)]
-    [string]$SelectedPackage,
+    [string]$SelectedNpmPackage,
 
     [parameter(Mandatory = $true)]
-    [string]$SelectedTag
+    [string]$SelectedNpmTag
 )
 
 # initialize
@@ -22,19 +22,19 @@ trap {
 }
 
 # find the artifact
-Set-TeamCityBuildNumber "$BuildCounter | $SelectedPackage"
-$artifact = @(Get-ChildItem build/$SelectedPackage*.tgz)
+Set-TeamCityBuildNumber "$BuildCounter | $SelectedNpmPackage"
+$artifact = @(Get-ChildItem build/$SelectedNpmPackage*.tgz)
 if ($artifact.Count -lt 1) {
-    Write-Error "No build artifact for `"$SelectedPackage`""
+    Write-Error "No build artifact for `"$SelectedNpmPackage`""
 }
 if ($artifact.Count -gt 1) {
-    Write-Error "Too many build artifacts for `"$SelectedPackage`""
+    Write-Error "Too many build artifacts for `"$SelectedNpmPackage`""
 }
 
 # extract version from artifact name
 $artifactBaseName = $artifact[0].BaseName
 $artifactPath = $artifact[0].FullName
-$artifactVersion = if ($artifactBaseName -match "^$($SelectedPackage)_(.+)$") {
+$artifactVersion = if ($artifactBaseName -match "^$($SelectedNpmPackage)_(.+)$") {
     $Matches[1]
 } else {
     $null
@@ -44,6 +44,6 @@ if (-not $artifactVersion) {
 }
 
 # persist results for subsequent build step(s)
-Write-TeamCityMessage "Build artifact for $SelectedPackage version $artifactVersion was found in `"$artifactPath`""
-Set-TeamCityBuildNumber "$BuildCounter | $SelectedPackage@$artifactVersion [$SelectedTag]"
+Write-TeamCityMessage "Build artifact for $SelectedNpmPackage version $artifactVersion was found in `"$artifactPath`""
+Set-TeamCityBuildNumber "$BuildCounter | $SelectedNpmPackage@$artifactVersion [$SelectedNpmTag]"
 Set-TeamCityParameter "packageBuildArtifactFullPath" $artifactPath
