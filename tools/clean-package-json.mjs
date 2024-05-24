5ab33b70631a95092eb6dd391838715678b1c254
Milan Gardian <mgardian@gmail.com>
2024-05-24 16:37:55 -0600

    Cleanup "package.json" during "yarn pack" ("prepack" script)

---
+++ tools/clean-package-json.mjs
@@ -0,0 +1,19 @@
+// Script run during packaging (see package.json) to tidy up the package.json
+// that is included with the released version.
+
+import { readFileSync, writeFileSync } from "node:fs";
+
+const packageJsonPath = "./package.json";
+const packageJsonString = readFileSync(packageJsonPath, "utf8");
+const packageJson = JSON.parse(packageJsonString);
+
+delete packageJson.browserslist;
+delete packageJson.devDependencies;
+delete packageJson.scripts;
+
+// console.log(packageJson);
+const newPackageJsonString = JSON.stringify(packageJson, null, 2) + "\n";
+if (packageJsonString !== newPackageJsonString) {
+  writeFileSync(packageJsonPath, newPackageJsonString, "utf8");
+  console.log("Cleaned package.json");
+}
