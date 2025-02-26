0838256f138c6d4b1cd9f9a2b0ff85216758db4b
Milan Gardian <milan.gardian@esgglobal.com>
2025-02-26 12:10:55 -0700

    Simplify "tsconfig.json"
    - Thanks JasonH (101100)

--- packages/eslint-config/src/eslint-plugin-react-hooks.d.ts
+++ packages/eslint-config/src/eslint-plugin-react-hooks.d.ts
@@ -1,9 +1,11 @@
+// No typings are available for "eslint-plugin-react-hooks@5.x.x" as of 2025-02-26.
+
 declare module "eslint-plugin-react-hooks" {
   import type { ESLint } from "eslint";
 
   export const configs: {
-    recommended: {
-      rules: {
+    readonly recommended: {
+      readonly rules: {
         "react-hooks/rules-of-hooks": "error";
         "react-hooks/exhaustive-deps": "warn";
       };
