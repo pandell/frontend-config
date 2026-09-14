023987444574e959b46427fcbf87330551dbb4a6
Milan Gardian <milan.gardian@esgglobal.com>
2026-09-14 17:07:36 -0600

    TeamCity: Switch Kotlin DSL to version 2026.2

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,7 @@ import lib.*
  * mvn --file .teamcity teamcity-configs:generate
  */
 
-version = "2026.1"
+version = "2026.2"
 
 // Prefixes of packages included in this monorepo.
 enum class NpmPackagePrefix {
